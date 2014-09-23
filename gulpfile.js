70ff55b3eab2e0163e357f342c2111f08b87f94a
Kevin Ngo <ngoke@mozilla.com>
2014-09-23 15:32:52 -0700

    requirejs inline config task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
+var insert = require("gulp-insert");
 var install = require("gulp-install");
 var _ = require('underscore');
 var config = require('./config');
+
+var paths = {
+    bower: 'bower_components/',
+};
+paths.require = paths.bower + 'requirejs/require.js';
 
 
 gulp.task('install', function(done) {
@@ -16,11 +22,22 @@ gulp.task('bower_copy', ['install'], function() {
     // Copy files from Bower into project.
     _.each(Object.keys(config.bowerConfig), function(source) {
         var dest = config.bowerConfig[source];
-        gulp.src('bower_components/' + source)
+        gulp.src(paths.bower + source)
             .pipe(gulp.dest(dest));
     });
 });
 
-gulp.task('update', ['bower_copy']);
+
+gulp.task('require_config', ['install'], function() {
+    // Build a require.js file that contains a convenience call to
+    // require.config that sets up some pre-known paths.
+    gulp.src(paths.require)
+        .pipe(insert.append(config.inlineRequireConfig))
+        .pipe(gulp.dest(config.LIB_DEST_PATH));
+});
+
+
+gulp.task('update', ['bower_copy', 'require_config']);
+
 
 gulp.task('default');
