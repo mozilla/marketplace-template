af5f038df925f19fbac2b05ebce3d48ce073b2c4
Kevin Ngo <ngoke@mozilla.com>
2014-09-26 14:20:44 -0700

    move to devdependencies, document the config

--- config.js
+++ config.js
@@ -1,7 +1,20 @@
+/* Configures component and module upgrade paths. */
 var config = require('commonplace').config;
 var extend = require('node.extend');
 
+var LIB_DEST_PATH = config.LIB_DEST_PATH;
+
 module.exports = extend(true, {
-    bowerConfig: {},
-    requireConfig: {}
+    bowerConfig: {
+        // Bower configuration for which files to get, and where to put them.
+        // [Source, excluding bower_components]: [Destination].
+        // 'isotope/dist/isotope.pkgd.js': config.LIB_DEST_PATH,
+    },
+    requireConfig: {
+        // RequireJS configuration for development, notably files in lib/.
+        // [Module name]: [Module path].
+        paths: {
+            // 'isotope': 'lib/isotope.pkgd',
+        }
+    }
 }, config);
