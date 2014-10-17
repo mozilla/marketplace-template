664ff4620f4b981095c4e4a7ded834545de03a59
Kevin Ngo <ngoke@mozilla.com>
2014-10-17 10:50:56 -0700

    update conifg template

--- config.js
+++ config.js
@@ -10,6 +10,15 @@ var localConfig = extend(true, {
         // [Source, excluding bower_components]: [Destination].
         // 'isotope/dist/isotope.pkgd.js': config.LIB_DEST_PATH,
     },
+    cssBundles: {
+        // Arbitrary CSS bundles to create.
+        // The key is the bundle name, which'll be excluded from the CSS build.
+        // 'splash.css': ['splash.styl.css']
+    },
+    cssExcludes: [
+        // List of CSS filenames to exclude from CSS build.
+        // splash.styl.css
+    ],
     requireConfig: {
         // RequireJS configuration for development, notably files in lib/.
         // [Module name]: [Module path].
