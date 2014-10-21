d8cef3e78a23229ddee2675af81cf7834ec44737
Kevin Ngo <ngoke@mozilla.com>
2014-10-21 12:36:58 -0700

    add a fabfile

---
+++ fabfile.py
@@ -0,0 +1,49 @@
+import os
+
+import fabdeploytools.envs
+from fabric.api import env, lcd, local, task
+from fabdeploytools import helpers
+
+import deploysettings as settings
+
+env.key_filename = settings.SSH_KEY
+fabdeploytools.envs.loadenv(settings.CLUSTER)
+
+ROOT, PROJECT_NAME = helpers.get_app_dirs(__file__)
+COMMONPLACE = '%s/node_modules/.bin/commonplace' % PROJECT_NAME
+
+if settings.ZAMBONI_DIR:
+    ZAMBONI = '%s/zamboni' % settings.ZAMBONI_DIR
+    ZAMBONI_PYTHON = '%s/venv/bin/python' % settings.ZAMBONI_DIR
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_local_mkt'
+os.environ["PATH"] += os.pathsep + os.pathsep.join([COMMONPLACE])
+
+
+@task
+def pre_update(ref):
+    with lcd(PROJECT_NAME):
+        local('git fetch')
+        local('git fetch -t')
+        local('git reset --hard %s' % ref)
+
+
+@task
+def update():
+    with lcd(PROJECT_NAME):
+        local('npm install')
+        local('node_modules/.bin/bower update --allow-root')
+        local('make update')
+        local('cp src/media/js/settings_local_hosted.js src/media/js/settings_local.js')
+        local('make build')
+        local('node_modules/.bin/commonplace langpacks')
+
+
+@task
+def deploy():
+    helpers.deploy(name=settings.PROJECT_NAME,
+                   app_dir='my-commonplace-app',
+                   env=settings.ENV,
+                   cluster=settings.CLUSTER,
+                   domain=settings.DOMAIN,
+                   root=ROOT)
