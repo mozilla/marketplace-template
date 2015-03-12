f286092bf1f1058b5b05db6c087a273e0f34d273
Kevin Ngo <ngoke@mozilla.com>
2015-03-12 16:43:12 -0700

    Revert "update settings local dist"
    This reverts commit 0638c79334aa1a02ee61f6f5c35e4b8302f9479c.

--- src/media/js/settings_local_hosted.js
+++ src/media/js/settings_local_hosted.js
@@ -1,7 +1,8 @@
 define('settings_local',
     ['core/polyfill'],
     function(polyfill) {
-    // core/polyfill needed for `window.location.origin`.
+
+    // core/polyfill is needed for `window.location.origin`.
     settings._extend({
         api_url: window.location.origin,
         media_url: document.body.getAttribute('data-media')
