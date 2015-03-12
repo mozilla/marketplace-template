6d19f45318b645049bab534660e32ea0aab88dd6
Kevin Ngo <ngoke@mozilla.com>
2015-03-12 16:38:47 -0700

    update settings local dist

--- src/media/js/settings_local_hosted.js
+++ src/media/js/settings_local_hosted.js
@@ -1,8 +1,7 @@
 define('settings_local',
     ['core/polyfill'],
     function(polyfill) {
-
-    // core/polyfill is needed for `window.location.origin`.
+    // core/polyfill needed for `window.location.origin`.
     settings._extend({
         api_url: window.location.origin,
         media_url: document.body.getAttribute('data-media')
